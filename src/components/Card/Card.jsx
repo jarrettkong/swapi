20886581100cceee1c455ba330799ed47d5022dd
Jarrett Kong <jarrettkong@me.com>
2019-04-30 15:49:32 -0600

    Refactor to include distinct promises

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const Card = props => {
-	const { name, homeworld, species } = props.result;
-	return (
-		<article className="Card">
-			<h2>{name}</h2>
-			<p>{species}</p>
-			<p>{homeworld}</p>
-		</article>
-	);
+class Card extends Component {
+	state = {
+		species: '',
+		homeworld: {
+			name: '',
+			population: 0
+		}
+	};
+
+	fetchHomeworld = () => {
+		return fetch(this.props.result.homeworld).then(res => res.json());
+	};
+
+	fetchSpecies = () => {
+		return fetch(this.props.result.species[0]).then(res => res.json());
+	};
+
+	componentDidMount() {
+		const speciesPromise = this.fetchSpecies();
+		const homeworldPromise = this.fetchHomeworld();
+
+		Promise.all([speciesPromise, homeworldPromise])
+			.then(data => {
+				this.setState({
+					species: data[0].name,
+					homeworld: {
+						name: data[1].name,
+						population: data[1].population
+					}
+				});
+			})
+			.catch(err => console.log(err));
+	}
+
+	render() {
+		const { name } = this.props.result;
+		const { species, homeworld } = this.state;
+		return (
+			<article className="Card">
+				<h2>{name}</h2>
+				<p>Species: {species}</p>
+				<p>Homeworld: {homeworld.name}</p>
+				<p>Homeworld Population: {homeworld.population}</p>
+			</article>
+		);
+	}
+}
+
+Card.propTypes = {
+	result: PropTypes.object.isRequired
 };
 
 export default Card;
