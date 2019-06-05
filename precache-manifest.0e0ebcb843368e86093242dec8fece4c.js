f90b0525d21e8e7f83571beb15d7db8896a34173
Jarrett Kong <jarrettkong@me.com>
2019-06-05 10:59:14 -0600

    Updates

---
+++ precache-manifest.0e0ebcb843368e86093242dec8fece4c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769428b3691db1c9779fe1f7f071a1d7",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "0c332e6c934698197124",
+    "url": "/swapi/static/css/2.d1f26340.chunk.css"
+  },
+  {
+    "revision": "fadd88890ed412c40928",
+    "url": "/swapi/static/css/main.bc862548.chunk.css"
+  },
+  {
+    "revision": "0c332e6c934698197124",
+    "url": "/swapi/static/js/2.de7bef2e.chunk.js"
+  },
+  {
+    "revision": "fadd88890ed412c40928",
+    "url": "/swapi/static/js/main.37ece014.chunk.js"
+  },
+  {
+    "revision": "86816fcca6b92ac284f8",
+    "url": "/swapi/static/js/runtime~main.3fb9cbf6.js"
+  },
+  {
+    "revision": "c1cb3ab9d4f7a4e5f10e546ab3fe735f",
+    "url": "/swapi/static/media/geonosis.c1cb3ab9.svg"
+  },
+  {
+    "revision": "86dde906061bda02532f27932dafb654",
+    "url": "/swapi/static/media/kylo_ren.86dde906.svg"
+  },
+  {
+    "revision": "9dca3b6658a85d75d7f99039356581fd",
+    "url": "/swapi/static/media/logo-transparent.9dca3b66.png"
+  },
+  {
+    "revision": "a4d41e98170aaf4837b8de5217c23448",
+    "url": "/swapi/static/media/x_wing.a4d41e98.svg"
+  }
+]);
